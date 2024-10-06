f8659c1bf800f1945085dfdfe5e5a878335a64b1
Hauke D <haukex@zero-g.net>
2024-10-06 17:04:22 +0000

    Updated devcontainer config

--- .devcontainer/initialize.sh
+++ .devcontainer/initialize.sh
@@ -5,5 +5,5 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"/..
 python -m venv .venv
 # shellcheck source=/dev/null
 source .venv/bin/activate
-python -m pip install --upgrade pip wheel
-pip install -r requirements.txt -r dev/requirements.txt
+make installdeps
+simple-perms -mr .
