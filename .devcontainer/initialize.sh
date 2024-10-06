3c7b6a894ba32ab9bef45119bacc242f67fe4fa7
Hauke D <haukex@zero-g.net>
2024-10-06 17:59:50 +0200

    Use `python` instead of `python3`
    Because venv doesn't always set up `python3`.

--- .devcontainer/initialize.sh
+++ .devcontainer/initialize.sh
@@ -2,8 +2,8 @@
 set -euxo pipefail
 cd "$( dirname "${BASH_SOURCE[0]}" )"/..
 
-python3 -m venv .venv
+python -m venv .venv
 # shellcheck source=/dev/null
 source .venv/bin/activate
-python3 -m pip install --upgrade pip wheel
+python -m pip install --upgrade pip wheel
 pip install -r requirements.txt -r dev/requirements.txt
